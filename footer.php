eec7a4a97d1a311d9dcc9eaaadd0ef6426dcd1ee
xeon365 <xee3oon@gmail.com>
2016-08-06 18:09:29 +0000

    First Commit

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
-<div id="footer">
-<h1>FOOTER</h1>
-</div>
-</div>
+            <div id="footer">
+                <h1>FOOTER</h1>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://77.98.146.170/testing/boot/js/bootstrap.min.js"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,10 +1,18 @@
-<html>
-<head>
-<title>Tutorial theme</title>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-</head>
-<body>
-<div id="wrapper">
-<div id="header">
-<h1>HEADER</h1>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Dot</title>
+        <link href="http://77.98.146.170/testing/boot/css/bootstrap.min.css" rel="stylesheet">
+        
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1>HEADER</h1>
+            </div>
+
+
